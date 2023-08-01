6b944d235e07712b415501d2c1fa9a6b61b76a5c
Bagas Wijayanto <baqazz@gmail.com>
2023-08-01 22:38:48 +0700

    add theme to app

--- lib/widgets/expense_list/expenses_list.dart
+++ lib/widgets/expense_list/expenses_list.dart
@@ -19,6 +19,12 @@ class ExpensesList extends StatelessWidget {
       child: ListView.builder(
         itemCount: expenses.length,
         itemBuilder: (context, index) => Dismissible(
+          background: Container(
+            color: Theme.of(context).colorScheme.error.withOpacity(0.7),
+            margin: EdgeInsets.symmetric(
+              horizontal: Theme.of(context).cardTheme.margin!.horizontal,
+            ),
+          ),
           onDismissed: (direction) => onRemoveExpense(
             expenses[index],
           ),
--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -49,13 +49,39 @@ class _ExpensesState extends State<Expenses> {
   }
 
   void _removeExpense(Expense expense) {
+    final expenseIndex = _listExpenses.indexOf(expense);
     setState(() {
       _listExpenses.remove(expense);
     });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        action: SnackBarAction(
+            label: 'Undo',
+            onPressed: () {
+              setState(() {
+                _listExpenses.insert(expenseIndex, expense);
+              });
+            }),
+        content: const Text('Expense deleted.'),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text('No Expense found'),
+    );
+
+    if (_listExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _listExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Expense Tracker'),
@@ -71,10 +97,8 @@ class _ExpensesState extends State<Expenses> {
           const Text('Chart'),
           const Text("List"),
           Expanded(
-              child: ExpensesList(
-            expenses: _listExpenses,
-            onRemoveExpense: _removeExpense,
-          ))
+            child: mainContent,
+          ),
         ],
       ),
     );
